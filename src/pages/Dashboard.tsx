69e1da29be70784d47aca6f0eda4376e4883e57e
JampierMoreno <67984118+jampier-santiago@users.noreply.github.com>
2023-06-01 18:31:27 -0500

    The endpoint for get all registers was finished

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,7 +2,13 @@
 import { FC } from "react";
 
 const Dashboard: FC = () => {
-  return <div>Dashboard</div>;
+  return (
+    <main className="dashboard">
+      <h1 className="dashboard__title">Todos los registros</h1>
+
+      <section></section>
+    </main>
+  );
 };
 
 export default Dashboard;
